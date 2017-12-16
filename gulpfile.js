4bd78398f84fb9a4556c5d109baa88ef83ec0710
Kyle Mayes <kyle@mayeses.com>
2017-12-16 02:02:35 -0500

    Add initial implementation

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const csso = require('gulp-csso');
+const ejs = require('gulp-ejs');
+const sass = require('gulp-sass');
+const util = require('gulp-util');
+
+gulp.task('build:html', () => {
+  return gulp.src('./source/index.html')
+    .pipe(ejs(require('./source/index.json')).on('error', util.log))
+    .pipe(gulp.dest('./target'));
+});
+
+gulp.task('build:scss', () => {
+  return gulp.src('./source/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(csso())
+    .pipe(gulp.dest('./target'));
+});
+
+gulp.task('build', ['build:html', 'build:scss']);
+
+gulp.task('watch', ['build'], () => {
+  return gulp.watch('./source/**/*', ['build']);
+});
